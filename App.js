71a2286f4887655ef9f4dc56f2718fbd1c61eaf8
Omar Sulehria and Pauline Cheung <osulehria@rallydev.com>
2015-01-29 11:03:36 -0800

    Initial commit

---
+++ App.js
@@ -0,0 +1,10 @@
+Ext.define('CrossWorkspaceReleaseBurndownApp', {
+    extend: 'Rally.app.App',
+    componentCls: 'app',
+    launch: function() {
+        //Write app code here
+        console.log('derp');
+
+        //API Docs: https://help.rallydev.com/apps/2.0/doc/
+    }
+});
