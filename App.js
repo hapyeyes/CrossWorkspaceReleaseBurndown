b89190ebb087125a52791524c961194320ad9e3c
Omar Sulehria <osulehria@rallydev.com>
2015-01-30 18:04:14 -0800

    Got cross-release dropdown working, need to cache workspace info to get workspace name

--- App.js
+++ App.js
@@ -10,50 +10,37 @@ Ext.define('CrossWorkspaceReleaseBurndownApp', {
 
     launch: function() {
         Deft.Chain.pipeline([
-            this.createFilteredReleaseStore,
+            this.getWorkspaceCollection,
+            this.getReleasesInWorkspaces,
+            this.filterLikeReleases,
             this.createFilteredCombobox
         ], this);
     },
 
-    createFilteredReleaseStore: function() {
-        var deferred = Ext.create('Deft.Deferred'),
-            releases = [],
-            me = this;
+    getWorkspaceCollection: function() {
+        var deferred = Ext.create('Deft.Deferred');
 
         Ext.create('Rally.data.wsapi.Store', {
-            model: 'Release',
+            model: 'Subscription',
+            fetch: ['Workspaces'],
             autoLoad: true,
-            context: {
-                workspace: null,
-                project: null
-            },
-            limit: Infinity,
             listeners: {
-                load: function(store, data) {
-                    _.map(data, function(release) {
-                        var rData = release.data;
-                        var likeRelease = me._isLikeReleaseInList(releases, rData.Name);
+                load: function (store, data) {
+                    var subscription = data[0];
 
-                        if (likeRelease) {
-                            likeRelease.ObjectID.push(rData.ObjectID);
-
-                            if (_.isEqual(likeRelease.Workspace, rData.Workspace)) {
-                                likeRelease.Workspace.push(rData.Workspace);
+                    subscription.getCollection('Workspaces').load({
+                        fetch: ['ObjectID', 'Name', 'State'],
+                        filters: [
+                            {
+                                property: 'State',
+                                operator: '!=',
+                                value: 'Closed'
                             }
-                        } else {
-                            releases.push({
-                                Name: rData.Name,
-                                ObjectID: [rData.ObjectID],
-                                Workspace: [rData.Workspace],
-                                ReleaseStartDate: rData.ReleaseStartDate,
-                                ReleaseDate: rData.ReleaseDate
-                            });
+                        ],
+                        callback: function(records) {
+                            deferred.resolve(records);
                         }
                     });
-
-                    releases = _.sortBy(releases, 'Name');
-                    console.log(releases.length);
-                    deferred.resolve(releases);
                 }
             }
         });
@@ -61,24 +48,78 @@ Ext.define('CrossWorkspaceReleaseBurndownApp', {
         return deferred.promise;
     },
 
-    createFilteredCombobox: function(filteredReleases) {
-        var combobox = Ext.create('Ext.form.field.ComboBox', {
-            fieldLabel: 'Choose Release',
-            store: Ext.create('Ext.data.Store', {
-                data: filteredReleases,
-                fields: ['Name', 'ObjectID']
-            }),
-            displayField: 'Name',
-            valueField: 'ObjectId',
-            width: 600,
-            listeners: {
-                select: function(combo, record) {
-                    var data = record[0].getData();
+    getReleasesInWorkspaces: function(workspaces) {
+        return Deft.Chain.parallel(_.map(workspaces, function(workspace) {
+            return function() {
+                var store = Ext.create('Rally.data.wsapi.Store', {
+                    model: 'Release',
+                    fetch: true,
+                    context: {
+                        workspace: Rally.util.Ref.getRelativeUri(workspace),
+                        project: null
+                    }
+                });
+
+                return store.load();
+            };
+        }));
+    },
+
+    filterLikeReleases: function(aggregateRecords) {
+        var deferred = Ext.create('Deft.Deferred'),
+            allReleases = _.pluck(_.flatten(aggregateRecords), 'data'),
+            releases = [],
+            me = this;
+
+        _.map(allReleases, function(release) {
+            var likeRelease = me._isLikeReleaseInList(releases, release.Name);
+
+            if (likeRelease) {
+                likeRelease.ObjectID.push(release.ObjectID);
+
+                if (!_.isEqual(likeRelease.Workspace, release.Workspace)) {
+                    likeRelease.Workspace.push(release.Workspace);
                 }
+            } else {
+                releases.push({
+                    Name: release.Name,
+                    ObjectID: [release.ObjectID],
+                    Workspace: [release.Workspace],
+                    ReleaseStartDate: release.ReleaseStartDate,
+                    ReleaseDate: release.ReleaseDate
+                });
             }
         });
 
-        this.add(combobox);
+        releases = _.sortBy(releases, 'Name');
+        deferred.resolve(releases);
+
+        return deferred.promise;
+    },
+
+    createFilteredCombobox: function(filteredReleases) {
+        this.add({
+            xtype: 'combobox',
+            fieldLabel: 'Choose Release',
+            store: Ext.create('Ext.data.Store', {
+                data: filteredReleases,
+                fields: ['Name', 'ObjectID', 'Workspace']
+            }),
+            displayField: 'Name',
+            valueField: 'ObjectId',
+            width: 300,
+            listeners: {
+                select: function(combo, record) {
+                    //var data = record[0].getData();
+                }
+            },
+            listConfig: {
+                itemTpl: new Ext.XTemplate(
+                    '<div class="release-name">{Name} &nbsp; &ndash; &nbsp;</div><div class="workspace-names">(<tpl for="Workspace">{Name}</tpl>)</div>'
+                ),
+                shadow: 'drop'
+            }
+        });
     },
 
     _isLikeReleaseInList: function(releases, releaseName) {
