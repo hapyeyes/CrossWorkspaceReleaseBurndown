7916c0bbf4ddaca668512da3c43625614199ed95
pcheung <pcheung@wrs.com>
2015-02-02 16:34:44 -0800

    Fix Workspace to only cache unique names; Release Drop Down now shows release start and release end date;

--- App.js
+++ App.js
@@ -1,130 +1,128 @@
 Ext.define('CrossWorkspaceReleaseBurndownApp', {
-    extend: 'Rally.app.App',
-    componentCls: 'app',
+	extend : 'Rally.app.App',
+	componentCls : 'app',
 
-    requires: [
-        'Deft.Deferred',
-        'Ext.form.field.ComboBox',
-        'Rally.data.wsapi.Store'
-    ],
+	requires : [ 'Deft.Deferred', 'Ext.form.field.ComboBox', 'Rally.data.wsapi.Store' ],
 
-    launch: function() {
-        Deft.Chain.pipeline([
-            this.getWorkspaceCollection,
-            this.getReleasesInWorkspaces,
-            this.filterLikeReleases,
-            this.createFilteredCombobox
-        ], this);
-    },
+	launch : function() {
+		Deft.Chain.pipeline([ this.getWorkspaceCollection, this.getReleasesInWorkspaces, this.filterLikeReleases, this.createFilteredCombobox ], this);
+	},
 
-    getWorkspaceCollection: function() {
-        var deferred = Ext.create('Deft.Deferred');
+	getWorkspaceCollection : function() {
+		var deferred = Ext.create('Deft.Deferred');
 
-        Ext.create('Rally.data.wsapi.Store', {
-            model: 'Subscription',
-            fetch: ['Workspaces'],
-            autoLoad: true,
-            listeners: {
-                load: function (store, data) {
-                    var subscription = data[0];
+		Ext.create('Rally.data.wsapi.Store', {
+			model : 'Subscription',
+			fetch : [ 'Workspaces' ],
+			autoLoad : true,
+			listeners : {
+				load : function(store, data) {
+					var subscription = data[0];
 
-                    subscription.getCollection('Workspaces').load({
-                        fetch: ['ObjectID', 'Name', 'State'],
-                        filters: [
-                            {
-                                property: 'State',
-                                operator: '!=',
-                                value: 'Closed'
-                            }
-                        ],
-                        callback: function(records) {
-                            deferred.resolve(records);
-                        }
-                    });
-                }
-            }
-        });
+					subscription.getCollection('Workspaces').load({
+						fetch : [ 'ObjectID', 'Name', 'State' ],
+						filters : [ {
+							property : 'State',
+							operator : '!=',
+							value : 'Closed'
+						} ],
+						callback : function(records) {
+							deferred.resolve(records);
+						}
+					});
+				}
+			}
+		});
 
-        return deferred.promise;
-    },
+		return deferred.promise;
+	},
 
-    getReleasesInWorkspaces: function(workspaces) {
-        return Deft.Chain.parallel(_.map(workspaces, function(workspace) {
-            return function() {
-                var store = Ext.create('Rally.data.wsapi.Store', {
-                    model: 'Release',
-                    fetch: true,
-                    context: {
-                        workspace: Rally.util.Ref.getRelativeUri(workspace),
-                        project: null
-                    }
-                });
+	getReleasesInWorkspaces : function(workspaces) {
+		return Deft.Chain.parallel(_.map(workspaces, function(workspace) {
+			return function() {
+				var store = Ext.create('Rally.data.wsapi.Store', {
+					model : 'Release',
+					fetch : true,
+					limit : Infinity,
+					context : {
+						workspace : Rally.util.Ref.getRelativeUri(workspace),
+						project : null
+					}
+				});
 
-                return store.load();
-            };
-        }));
-    },
+				return store.load();
+			};
+		}));
+	},
 
-    filterLikeReleases: function(aggregateRecords) {
-        var deferred = Ext.create('Deft.Deferred'),
-            allReleases = _.pluck(_.flatten(aggregateRecords), 'data'),
-            releases = [],
-            me = this;
+	filterLikeReleases : function(aggregateRecords) {
+		var deferred = Ext.create('Deft.Deferred'), allReleases = _.pluck(_.flatten(aggregateRecords), 'data'), releases = [], me = this;
 
-        _.map(allReleases, function(release) {
-            var likeRelease = me._isLikeReleaseInList(releases, release.Name);
+		_.map(allReleases, function(release) {
+			var likeRelease = me._isLikeReleaseInList(releases, release.Name);
 
-            if (likeRelease) {
-                likeRelease.ObjectID.push(release.ObjectID);
+			if (likeRelease) {
+				likeRelease.ObjectID.push(release.ObjectID);
+				if (!me._isLikeReleaseWorkspaceInList(likeRelease.Workspace, release.Workspace._refObjectName)) {
+					likeRelease.Workspace.push(release.Workspace);
+				}
 
-                if (!_.isEqual(likeRelease.Workspace, release.Workspace)) {
-                    likeRelease.Workspace.push(release.Workspace);
-                }
-            } else {
-                releases.push({
-                    Name: release.Name,
-                    ObjectID: [release.ObjectID],
-                    Workspace: [release.Workspace],
-                    ReleaseStartDate: release.ReleaseStartDate,
-                    ReleaseDate: release.ReleaseDate
-                });
-            }
-        });
+			} else {
+				var formattedStartDate = Rally.util.DateTime.formatWithDefault(release.ReleaseStartDate);
 
-        releases = _.sortBy(releases, 'Name');
-        deferred.resolve(releases);
+				var shiftedEndDate = Rally.util.DateTime.shiftTimezoneOffDate(release.ReleaseDate);
+				var formattedEndDate = Rally.util.DateTime.formatWithDefault(shiftedEndDate);
 
-        return deferred.promise;
-    },
+				releases.push({
+					Name : release.Name,
+					ObjectID : [ release.ObjectID ],
+					Workspace : [ release.Workspace ],
+					ReleaseStartDate : formattedStartDate,
+					ReleaseDate : formattedEndDate
+				});
+			}
+		});
 
-    createFilteredCombobox: function(filteredReleases) {
-        this.add({
-            xtype: 'combobox',
-            fieldLabel: 'Choose Release',
-            store: Ext.create('Ext.data.Store', {
-                data: filteredReleases,
-                fields: ['Name', 'ObjectID', 'Workspace']
-            }),
-            displayField: 'Name',
-            valueField: 'ObjectId',
-            width: 300,
-            listeners: {
-                select: function(combo, record) {
-                    //var data = record[0].getData();
-                }
-            },
-            listConfig: {
-                itemTpl: new Ext.XTemplate(
-                    '<div class="release-name">{Name} &nbsp; &ndash; &nbsp;</div><div class="workspace-names">(<tpl for="Workspace">{Name}</tpl>)</div>'
-                ),
-                shadow: 'drop'
-            }
-        });
-    },
+		releases = _.sortBy(releases, 'Name');
+		deferred.resolve(releases);
 
-    _isLikeReleaseInList: function(releases, releaseName) {
-        return _.find(releases, function(release) {
-            return release.Name === releaseName
-        });
-    }
+		return deferred.promise;
+	},
+
+	createFilteredCombobox : function(filteredReleases) {
+		debugger;
+		this.add({
+			xtype : 'combobox',
+			fieldLabel : 'Choose Release',
+			store : Ext.create('Ext.data.Store', {
+				data : filteredReleases,
+				fields : [ 'Name', 'ObjectID', 'Workspace', 'ReleaseStartDate', 'ReleaseDate' ]
+			}),
+			displayField : 'Name',
+			valueField : 'ObjectId',
+			width : 600,
+			listeners : {
+				select : function(combo, record) {
+					// var data = record[0].getData();
+				}
+			},
+			listConfig : {
+				itemTpl : new Ext.XTemplate('<div class="release-name">{Name} &nbsp; &ndash; &nbsp; </div>'
+						+ '<div class="workspace-names">( <tpl for="Workspace">{_refObjectName} </tpl>)</div>'
+						+ '<div class="release-dates">{ReleaseStartDate} &nbsp; &ndash; &nbsp; {ReleaseDate}</div>'),
+				shadow : 'drop'
+			}
+		});
+	},
+
+	_isLikeReleaseInList : function(releases, releaseName) {
+		return _.find(releases, function(release) {
+			return release.Name === releaseName;
+		});
+	},
+	_isLikeReleaseWorkspaceInList : function(workspaces, workspaceName) {
+		return _.find(workspaces, function(workspace) {
+			return workspace._refObjectName === workspaceName;
+		});
+	}
 });
